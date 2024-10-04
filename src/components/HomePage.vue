80afe675c28698f9de4a162cb6cd991ca57ece6c
Matthieu <matthew.ramanan@gmail.com>
2024-10-04 12:35:36 +0200

    Ex 7 : Create BaseButton with primary color

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,15 +1,32 @@
 <template>
   <div>
     <h1>Welcome to the Home Page</h1>
+
+    <!-- Button with custom margin -->
+    <BaseButton customClass="custom-margin">
+      BaseButton with custom margin
+    </BaseButton>
+
+    <!-- Disabled button -->
+    <BaseButton :disabled="true">
+      BaseButton disabled
+    </BaseButton>
   </div>
 </template>
 
 <script>
+import BaseButton from './BaseButton.vue'
+
 export default {
-  name: 'HomePage'
+  name: 'HomePage',
+  components: {
+    BaseButton
+  }
 }
 </script>
 
 <style scoped>
-
+.custom-margin {
+  margin: 20px;
+}
 </style>

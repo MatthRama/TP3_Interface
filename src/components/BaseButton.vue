08c0e01ac0d208b8af87ba1b5bc4ad824913a6a5
Matthieu <matthew.ramanan@gmail.com>
2024-10-04 12:35:46 +0200

    Ex 7 : Create BaseButton with primary color

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,67 @@
+<template>
+  <button
+      :class="['base-button', { 'base-button--disabled': disabled }, customClass]"
+      :disabled="disabled"
+      :type="type"
+      :role="role"
+  >
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'BaseButton',
+  props: {
+    type: {
+      type: String,
+      default: 'button',  // Can be 'button', 'submit', etc.
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+    role: {
+      type: String,
+      default: 'button',
+    },
+    customClass: {
+      type: String,
+      default: '',  // Additional class for custom styling
+    }
+  }
+}
+</script>
+
+<style scoped>
+.base-button {
+  background-color: green;
+  color: white;
+  padding: 10px 20px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  font-size: 16px;
+  transition: background-color 0.3s ease, transform 0.3s ease;
+}
+
+.base-button:hover {
+  background-color: #28a745;
+  transform: scale(1.05);
+}
+
+.base-button:focus {
+  outline: none;
+  box-shadow: 0 0 5px rgba(0, 0, 0, 0.3);
+  transform: scale(1.05);
+}
+
+.base-button--disabled {
+  background-color: gray;
+  cursor: not-allowed;
+}
+
+.base-button:disabled {
+  background-color: gray;
+}
+</style>
